2dfde706c1946a311004285d9b3de6b735f0a567
JonMS95 <jon_portugalete@hotmail.com>
2022-11-26 19:08:58 +0100

    Added a new constant.

--- Python_File_handling/fileHandling_constants.py
+++ Python_File_handling/fileHandling_constants.py
@@ -1,2 +1,3 @@
 PATH_STRANGE_MAN    = "/home/jon/Desktop/scripts/Python/Python_File_handling/strangeMan.txt"
 PATH_FIRE           = "/home/jon/Desktop/scripts/Python/Python_File_handling/fire.txt"
+LINE_BLACKWATER     = "I know what you did in Blackwater."
