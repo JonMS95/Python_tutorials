195a9b2c0998be56dd081cfbadb53c86828781db
JonMS95 <jon_portugalete@hotmail.com>
2022-11-26 17:22:27 +0100

    New file added to repository. How to use PIP is explained.

---
+++ Python_PIP/importUsingPIP.py
@@ -0,0 +1,15 @@
+# First, check if PIP is installed in your machine. Just type pip --version.
+# If it's already installed, the version
+# will be displayed.
+# If that's not the case, go ahead and install it by using the "sudo apt install python-pip" command.
+# Upgrading it may be required as well. To do so, use the "sudo pip install --upgrade pip" command.
+# If any version of Python3 is already installed, it's strongly recommended to use sudo apt install python3-pip
+# instead of any of the commands mentioned above.
+
+# Once the Linux PIP package is installed and upgraded, it can be used to download python open-source modules
+# from a large library. To find them, clock on the following link:
+# https://pypi.org/
+
+# In this case, a simple string encryption decryption library is going to be installed: https://pypi.org/project/scryp/.
+# Type "pip3 install scryp" in the linux console to install it.
+
