4888f180f20eba9a0d37b5a547baf24fa1c15738
JonMS95 <jon_portugalete@hotmail.com>
2022-12-06 16:44:29 +0100

    Created source file and compilated it as a dynamic library.

---
+++ Python_C_functions/checkIntSign.c
@@ -0,0 +1,6 @@
+// The function below returns 0 if the number is equal to zero, 1 if it's positive, or -1 if it's negative.
+
+int function(int input)
+{
+    return (input == 0 ? 0 : (input > 0 ? 1 : -1));
+}
