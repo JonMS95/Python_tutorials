9582801cd5b6bbf7098f5cf19c71011fd889535d
JonMS95 <jon_portugalete@hotmail.com>
2022-12-17 17:44:44 +0100

    WIP

---
+++ Python_Lambda/lambda.py
@@ -0,0 +1,8 @@
+# Same as in other OOP languages such as C++, lambda functions are just small anonymous functions. The syntax is as follows:
+# lambda arguments : expression
+x = lambda a : a + 10
+print(x(5))
+
+# Same as common functions, lambda functions accept multiple input arguments:
+y = lambda a, b : a + b
+print(y(4, 7))
