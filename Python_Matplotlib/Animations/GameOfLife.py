4f3acf05af09f06af94b8f5f565dd3a3e6cb8c28
JonMS95 <jon_portugalete@hotmail.com>
2023-05-13 17:06:27 +0200

    Added changes in cellNextState for the case where loop==True, modified function to use class member board, instead of an external one.

--- Python_Matplotlib/Animations/GameOfLife.py
+++ Python_Matplotlib/Animations/GameOfLife.py
@@ -2,30 +2,41 @@ from typing import List
 from random import randint
 
 class GameOfLife:
-    def __init__(self, rows:int, columns:int):
+    def __init__(self, rows:int, columns:int, loop:bool):
         self.board = [[randint(0, 1) for j in range(columns)] for i in range(rows)]
-        # self.board = [[0] * columns for i in range(rows)]
-        # self.board[int(rows/2)][int(columns/2)] = 1
+        self.loop = loop
 
-    def cellNextState(self, i, j, board: List[List[int]]) -> bool:
-        i_min = i if i == 0 else i - 1
-        i_max = i if i == len(board) - 1 else i + 1
-        j_min = j if j == 0 else j - 1
-        j_max = j if j == len(board[0]) - 1 else j + 1
-
+    def cellNextState(self, i, j) -> bool:
         counter = 0
 
-        for line in range(i_min, i_max + 1, 1):
-            for col in range(j_min, j_max + 1, 1):
-                if line == i and col == j:
-                    continue
-                if board[line][col] == 1:
-                    counter += 1
-        
-        if board[i][j] == 0 and counter == 3:
+        if self.loop == False:        
+            i_min = i if i == 0 else i - 1
+            i_max = i if i == self.getBoardSize()[0] - 1 else i + 1
+            j_min = j if j == 0 else j - 1
+            j_max = j if j == self.getBoardSize()[1] - 1 else j + 1
+
+            for line in range(i_min, i_max + 1, 1):
+                for col in range(j_min, j_max + 1, 1):
+                    if line == i and col == j:
+                        continue
+                    if self.board[line][col] == 1:
+                        counter += 1
+        else:
+            print("Check cell in row {}, col{}".format(i, j))
+            for line in range(i - 1, i + 2, 1):
+                for col in range(j - 1, j + 2, 1):
+                    actual_line = line % self.getBoardSize()[0]
+                    actual_col  = col  % self.getBoardSize()[1]
+                    print("Actual line = {}\r\nActual column = {}".format(actual_line, actual_col))
+                    if actual_line == i and actual_col == j:
+                        continue
+                    if self.board[actual_line][actual_col] == 1:
+                        counter += 1
+
+        if self.board[i][j] == 0 and counter == 3:
             return True
 
-        if board[i][j] == 1 and (counter < 2 or counter > 3):
+        if self.board[i][j] == 1 and (counter < 2 or counter > 3):
             return True
 
         return False
@@ -35,7 +46,7 @@ class GameOfLife:
 
         for i in range(0, len(self.board), 1):
             for j in range(0, len(self.board[0]), 1):
-                if self.cellNextState(i, j, self.board) == True:
+                if self.cellNextState(i, j) == True:
                     changes.append([i, j])
 
         for k in changes:
