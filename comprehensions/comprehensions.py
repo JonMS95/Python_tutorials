463228f3c404572409d091cca016c9b9ecfc9e91
Jon <jon_portugalete@hotmail.com>
2025-11-04 23:54:04 +0100

    Added some explanation on comprehensions in python.

--- comprehensions/comprehensions.py
+++ comprehensions/comprehensions.py
@@ -1,3 +1,14 @@
+'''
+A comprehension is a compact syntax for creating new lists, sets, or dictionaries from existing iterables.
+They’re faster, cleaner, and idiomatic.
+
+Comprehensions are not just syntactic sugar: they’re slightly faster than loops because the iteration
+happens in C-level code internally. Use them as much as possible, preferably when initializing data collection
+types (lists, sets dicts and tuples).
+
+Take a look at the examples scattered across this module to have a better idea about how to use them properly.
+'''
+
 from list_comprehensions    import callListCompFns
 from dict_comprehensions    import callDictCompFns
 from set_comprehensions     import callSetCompFns
