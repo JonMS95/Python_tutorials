2c3d8fe9282a17b64e13ea35ae2c165018cc6e8c
Jon <jon_portugalete@hotmail.com>
2025-11-04 23:50:16 +0100

    Main module of the lesson.

---
+++ comprehensions/comprehensions.py
@@ -0,0 +1,13 @@
+from list_comprehensions    import callListCompFns
+from dict_comprehensions    import callDictCompFns
+from set_comprehensions     import callSetCompFns
+from generator_expressions  import callGenExpFns
+
+def main():
+    callListCompFns()
+    callDictCompFns()
+    callSetCompFns()
+    callGenExpFns()
+
+if __name__ == "__main__":
+    main()
