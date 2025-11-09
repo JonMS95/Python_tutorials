d3abf220ff10d2e74fbff665245c7958b5022785
Jon <jon_portugalete@hotmail.com>
2025-11-09 19:02:26 +0100

    Added more ops.

--- Testing_Tools/math_ops.py
+++ Testing_Tools/math_ops.py
@@ -6,8 +6,14 @@ from typing import Union
 
 num_type = Union[int, float]
 
+def add(a: num_type, b: num_type) -> num_type:
+    return (a + b)
+
+def mul(a: num_type, b: num_type) -> num_type:
+    return (a * b)
+
 def sub(a: num_type, b: num_type) -> num_type:
     return (a - b)
 
 def div(a: num_type, b: num_type) -> num_type:
-    return a / b    # Raises ZeroDivisionError by itself if necessary. 
+    return (a / b)    # Raises ZeroDivisionError by itself if necessary.
