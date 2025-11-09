e7944267cac022a1ca3d8bd6dd40282169fe9d85
Jon <jon_portugalete@hotmail.com>
2025-11-09 13:02:11 +0100

    Removed old files, replaced them by fresher, updated lessons.

--- Python_File_handling/fileHandling_constants.py
+++
@@ -1,8 +0,0 @@
-PATH_TO_PROJECT     = "/home/jon/Desktop/scripts/Python/Python_File_handling"
-PATH_STRANGE_MAN    = PATH_TO_PROJECT + "/strangeMan.txt"
-PATH_FIRE           = PATH_TO_PROJECT + "/fire.txt"
-PATH_I_KNOW_YOU     = PATH_TO_PROJECT + "/I_KNOW_YOU.txt"
-
-LINE_BLACKWATER     = "\nI know what you did in Blackwater."
-
-PASSWORD            = "JIM MILTON"
