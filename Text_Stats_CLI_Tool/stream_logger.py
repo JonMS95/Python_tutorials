fd9d9dea0f867013b0af43aa6d05c248c9bbec0d
Jon <jon_portugalete@hotmail.com>
2025-11-16 18:59:28 +0100

    Logging module.

---
+++ Text_Stats_CLI_Tool/stream_logger.py
@@ -0,0 +1,36 @@
+import logging
+from logging import DEBUG as dbg, INFO  as inf, WARNING as wng, ERROR as err, CRITICAL as crt
+
+class StreamLogger:
+    log_format: str = "%(asctime)s - %(filename)s - %(levelname)s - %(message)s"
+    logger = None
+
+    def __init__(self, name: str = __name__, level: int = logging.INFO):
+        self.handler = logging.StreamHandler()
+        self.handler.setFormatter(logging.Formatter(self.log_format))
+
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+
+        self.logger.addHandler(self.handler)
+    
+
+    def logDbg(self, msg: str = "") -> None:
+        self.logger.debug(msg)
+    
+
+    def logInf(self, msg: str = "") -> None:
+        self.logger.info(msg)
+    
+
+    def logWng(self, msg: str = "") -> None:
+        self.logger.warning(msg)
+    
+
+    def logErr(self, msg: str = "") -> None:
+        self.logger.error(msg)
+    
+    
+    def logCrt(self, msg: str = "") -> None:
+        self.logger.critical(msg)
+    
