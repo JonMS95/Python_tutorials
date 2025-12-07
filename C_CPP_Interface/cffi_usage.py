9611582b40a981794f4c80de21820623cbf9a5ea
Jon <jon_portugalete@hotmail.com>
2025-12-07 19:48:20 +0100

    Created cffi explaining module file.

---
+++ C_CPP_Interface/cffi_usage.py
@@ -0,0 +1,11 @@
+'''
+cffi is an alternative to ctypes. Altough it's not Python's "native" format, it's better
+when comlex structs or pointer manipulation is involved.
+
+The following command may be required so as to install required package(s):
+pip install cffi
+'''
+
+from cffi import FFI
+from timing_decorator import timingDecorator as t_deco
+
