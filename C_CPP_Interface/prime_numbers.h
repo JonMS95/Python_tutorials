44fca4799812220b9dd3c83ec6c04007328e80c8
Jon <jon_portugalete@hotmail.com>
2025-12-07 17:18:03 +0100

    Added C files for a dynamic library to be created.

---
+++ C_CPP_Interface/prime_numbers.h
@@ -0,0 +1,17 @@
+#ifndef PRIME_NUMBERS
+#define PRIME_NUMBERS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PRIME_NUMBERS_API   __attribute__((visibility("default")))
+
+PRIME_NUMBERS_API bool isPrime(const int n);
+PRIME_NUMBERS_API void printPrimeNumbersInRange(const int range_start, const int range_end);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
