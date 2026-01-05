806ef2e247c18c192b670e6a2046d9962ac957d6
JonMS95 <jon_portugalete@hotmail.com>
2026-01-05 19:17:56 +0100

    Added a function explaining how to handle copies in NumPy.

--- NumPy_Fundamentals/numpy_memory_management.py
+++ NumPy_Fundamentals/numpy_memory_management.py
@@ -21,13 +21,33 @@ content of the pointed memory blocks.
 def viewsinNumPy() -> None:
     x = np.array([_**2 for _ in range(10)])
     x_view = x[1:9]
+
     print("x: ", x)
     print("x_view: ", x_view)
+    
     x_view[0] = 100
+    
     print("After modifying view (x_view[0] = 100), x: ", x)
+
+'''
+Copies create completely new arrays, stored in new memory blocks. Thus,
+modifying the copy leaves the original array unmodified.
+'''
+def copiesInNumpy(input: np.array) -> None:
+    x = input.copy()
+    y = input[0:(input.size - 1)].copy()
+    
+    x[0] = 1
+    y[-1] = 777
+
+    print(f"x: {x}")
+    print(f"y: {y}")
+    print(f"input: {input}")
 
 def main():
     viewsinNumPy()
+    input = np.array([_ * 2 for _ in range(10)])
+    copiesInNumpy(input)
 
 if __name__ == "__main__":
     main()
