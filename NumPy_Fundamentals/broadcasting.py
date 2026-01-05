15fda48dd1c541df9ee8382370d618a01d0ed8f2
JonMS95 <jon_portugalete@hotmail.com>
2026-01-05 19:41:07 +0100

    Added a basic example explaining how briadcasting works in Python.

--- NumPy_Fundamentals/broadcasting.py
+++ NumPy_Fundamentals/broadcasting.py
@@ -9,3 +9,16 @@ There's no way y can be added to x. However, there's a mechanism namely NumPy
 that can shorten the path to perform the application so taht there's no need
 to generate an array of 5's explictly then add it to the NumPy array. 
 '''
+
+import numpy as np
+
+def basicBroadcastingSum(x: np.array = np.array([_ for _ in range(1, 5)]), y = 5) -> np.array:
+    z = x + y
+    print(f"{x} + {y} = {z}")
+    return (x + y)
+
+def main():
+    basicBroadcastingSum()
+
+if __name__ == "__main__":
+    main()
