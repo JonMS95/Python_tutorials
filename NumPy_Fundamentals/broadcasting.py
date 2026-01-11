3b7499b480385414f971ec7005e21f2a259fcbb6
JonMS95 <jon_portugalete@hotmail.com>
2026-01-11 19:44:35 +0100

    Added more examples.

--- NumPy_Fundamentals/broadcasting.py
+++ NumPy_Fundamentals/broadcasting.py
@@ -17,8 +17,26 @@ def basicBroadcastingSum(x: np.ndarray = np.array([_ for _ in range(1, 5)]), y =
     print(f"{x} + {y} = {z}")
     return (x + y)
 
+def scalarToVector() -> np.ndarray:
+    x: np.ndarray = np.arange(10, 50, 10)
+    y: int = 5
+    z: np.ndarray = x + y
+
+    print(f"Scalar to vector broadcasting: {x} + {y} = {z}")
+    return z
+
+def vectorToMatrix() -> np.ndarray:
+    X: np.ndarray = np.arange(1, 7).reshape(2, 3)   # Creates a 1-D vector of numbers from 1 to 6 (inclusive), then it rearranges numbers into a 2 x 3 matrix.
+    y: np.ndarray = np.array([10, 20, 30])          # Creates a 1-D vector with given elements.
+    C: np.ndarray = X + y
+
+    print(f"Vector to matrix broadcasting:\r\nX: {X}\r\ny: {y}\r\nC: {C}")
+    return C
+
 def main():
     basicBroadcastingSum()
+    scalarToVector()
+    vectorToMatrix()
 
 if __name__ == "__main__":
     main()
