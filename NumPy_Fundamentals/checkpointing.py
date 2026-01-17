ec31787b5b373a4586ec65a65c9b05aa4c32f688
JonMS95 <jon_portugalete@hotmail.com>
2026-01-17 17:58:04 +0100

    Added a file about checkpointing in NumPy.

---
+++ NumPy_Fundamentals/checkpointing.py
@@ -0,0 +1,11 @@
+'''
+This chapter is not about math but rather about engineering discipline. Checkpointing is
+a NumPy feature (it's also featured in common Python) that allows us to save the status
+of a program so that it can be backed up so as to continue later with it or simply to
+save critical data.
+
+Similar to picle, .save (.savez and .savez_compressed for multiple arrays) and .load
+functions are used whe in NumPy, being .npy its format.
+'''
+
+import numpy as np
