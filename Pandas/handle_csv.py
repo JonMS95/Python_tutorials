cf71458201955c995fe26a13dba8c3a308ab1b02
JonMS95 <jon_portugalete@hotmail.com>
2026-01-18 17:45:57 +0100

    Added a file to dive deep into the nuances of CSV handling with Pandas module.

---
+++ Pandas/handle_csv.py
@@ -0,0 +1,27 @@
+'''
+Pandas can handle CSV files. In our case, we will be using a sample light database for such purpose.
+A CSV file is nothing but a plain text file in which data is being separated by using newlines for rows
+and commas for columns. It has no inherent types nor schema by nature. Therefore, when Pandas reads a
+CSV file, it must parse text, infer structure, guess data types and normalize missing values. It's
+worth pointing out that most bugs come from trusting these guesses blindly.
+'''
+
+import pandas as pd
+from pathlib import Path, PosixPath
+from os import getcwd
+
+def readCSVFromPath(csv_file_path: str = (getcwd() + "/test_data.csv")) -> pd.DataFrame:
+    print(csv_file_path)
+    p: PosixPath = Path(csv_file_path)
+    
+    if not p.exists():
+        raise FileNotFoundError(f"Target CSV file path does not exist ({csv_file_path})")
+
+    return pd.read_csv(csv_file_path)    
+
+def main():
+    df: pd.DataFrame = readCSVFromPath()
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
