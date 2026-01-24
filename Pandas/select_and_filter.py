824fb26f7ee4ce69e5704beaad4a7cf162951652
JonMS95 <jon_portugalete@hotmail.com>
2026-01-24 17:46:38 +0100

    Distributed the lesson into simpler functions.

--- Pandas/select_and_filter.py
+++ Pandas/select_and_filter.py
@@ -7,10 +7,7 @@ other lessons.
 from handle_csv import readCSVFromPath, nl
 import pandas as pd
 
-def selectAndFilter() -> None:
-    # Retrieve a Pandas DataFrame object from a CSV file.
-    df: pd.DataFrame = readCSVFromPath()
-
+def basicSelection(df: pd.DataFrame) -> None:
     # Get just an isolated column from the DatFrame object as Pandas Series object.
     emails: pd.Series = df["Email"]
     print(f"type(emails): {type(emails)}")
@@ -23,7 +20,8 @@ def selectAndFilter() -> None:
     id: pd.DataFrame = df[["Customer Id"]]
     print(f"type(id): {type(id)}")
 
-    # "iloc" stands for integer location (purely positional). Returns a row as a Pandas Series object if only a single row is selected. 
+def ilocUsage(df: pd.DataFrame) -> None:
+        # "iloc" stands for integer location (purely positional). Returns a row as a Pandas Series object if only a single row is selected. 
     first_row: pd.Series = df.iloc[0]
     print(f"type(first_row): {type(first_row)}")
 
@@ -36,11 +34,20 @@ def selectAndFilter() -> None:
     cell: str = df.iloc[0, 2]
     print(f"cell at [0, 2]: {cell}")
 
+def locUsage(df: pd.DataFrame) -> None:
     # Use "loc" to select more generic DataFrames.
     loc_data: pd.Series = df.loc[10]                                        # Solely 10th row.
     loc_data_row_and_col: pd.Series = df.loc[10, "Email"]                   # Selecting a single cell (10th row from "Email"-labeled column).
     loc_data_subset: pd.DataFrame = df.loc[0:4, ["Customer Id", "Email"]]   # Select a range of rows out of some selected columns.
     print(f"loc_data_subset (columns: {loc_data_subset.columns}, rows 0 to 4):{nl}{loc_data_subset}")
+
+def selectAndFilter() -> None:
+    # Retrieve a Pandas DataFrame object from a CSV file.
+    df: pd.DataFrame = readCSVFromPath()
+
+    basicSelection(df)
+    ilocUsage(df)
+    locUsage(df)
 
 def main():
     selectAndFilter()
