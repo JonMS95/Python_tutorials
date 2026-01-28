ce98b6645181650aa27cb627b1d0efae5be4fa0d
JonMS95 <jon_portugalete@hotmail.com>
2026-01-28 19:10:38 +0100

    Added new file.

---
+++ Pandas/data_transformation.py
@@ -0,0 +1,10 @@
+'''
+This file will cover data transformation in Pandas. Topics to be covered include new column creation,
+conditional columns, string transformations, applying custom logic and mapping values.
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
