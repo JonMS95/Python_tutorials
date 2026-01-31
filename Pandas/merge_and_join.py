dd69a60834dfaa11d6e5bd1d0283174bc233aaba
JonMS95 <jon_portugalete@hotmail.com>
2026-01-31 16:54:07 +0100

    Added some basic DataFrames to operate with.

--- Pandas/merge_and_join.py
+++ Pandas/merge_and_join.py
@@ -6,6 +6,16 @@ being imported from CSV files so as to make them more understandable.
 
 import pandas as pd
 
+df_cusomters: pd.DataFrame = pd.DataFrame({
+    "Customer ID"   :   [0, 1, 2, 3]                        ,
+    "Name"          :   ["Alice", "Bob", "Charlie", "Daisy"],
+})
+
+df_subscriptions: pd.DataFrame = pd.DataFrame({
+    "Customer ID"   :   [1, 3]              ,
+    "Plan"          :   ["Premium", "Basic"],
+})
+
 def main():
     pass
 
