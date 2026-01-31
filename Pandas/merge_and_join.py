8912e8fadde5d4ffbb7a6377187328daf1494568
JonMS95 <jon_portugalete@hotmail.com>
2026-01-31 16:50:35 +0100

    Added a file about merging in pandas.

---
+++ Pandas/merge_and_join.py
@@ -0,0 +1,13 @@
+'''
+This file will include some examples covering Pandas .merge() method (equivalent to SQL's JOIN).
+Different from other files in this Pandas chapter, DataFrames will be fully custom instead of
+being imported from CSV files so as to make them more understandable.
+'''
+
+import pandas as pd
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
