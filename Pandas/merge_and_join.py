117367187e6e47ca785599f3bd374136b414afd7
JonMS95 <jon_portugalete@hotmail.com>
2026-01-31 16:55:25 +0100

    Added a comment telling how .merge() usage examples will be organized.

--- Pandas/merge_and_join.py
+++ Pandas/merge_and_join.py
@@ -2,6 +2,8 @@
 This file will include some examples covering Pandas .merge() method (equivalent to SQL's JOIN).
 Different from other files in this Pandas chapter, DataFrames will be fully custom instead of
 being imported from CSV files so as to make them more understandable.
+
+Examples below will refer to merge use cases as in SQL: INNER JOIN, LEFT/RIGHT JOIN, FULL OUTER JOIN.
 '''
 
 import pandas as pd
