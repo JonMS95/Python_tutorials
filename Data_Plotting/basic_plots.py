115d358df406726118c738f284cbd4a02c5d014f
JonMS95 <jon_portugalete@hotmail.com>
2026-01-31 19:34:26 +0100

    Organized plotting in two different functions called by the main one (a function per method).

--- Data_Plotting/basic_plots.py
+++ Data_Plotting/basic_plots.py
@@ -11,12 +11,7 @@ matplotlib.use("TkAgg")  # Must be before pyplot
 import matplotlib.pyplot as plt
 import numpy as np
 
-def dummyExample() -> None:
-    # Generate data to be plotted.
-    x: np.ndarray   = np.linspace(0, 10, 100)   # Generate 100 evenly spaced data points from 0 to 10.
-    y0: np.ndarray  = np.sin(x)                 # Execute sine function over points generated above.
-    y1: np.ndarray  = np.cos(x)                 # Same for cosine function.
-
+def quickPlotting(x: np.ndarray, y0: np.ndarray, y1: np.ndarray) -> None:
     # Create a figure for plotting
     plt.figure(figsize = (8, 4))    # Width: 8, Height: 4 (both in inches!).
 
@@ -33,6 +28,34 @@ def dummyExample() -> None:
     plt.tight_layout()
     # plt.savefig("line_plot_quick.png")    # Uncomment to save generated plot as png file.
     plt.show()
+
+def plotWithAPI(x: np.ndarray, y0: np.ndarray, y1: np.ndarray) -> None:
+    # Creates both a figure (like a canvas, containing axes, titles and plots) and ax (axes) is where the actual plot lives in.
+    fig, ax = plt.subplots(figsize = (8, 4))
+    
+    # Plot both functions.
+    ax.plot(x, y0, label = "sin(x)", color = "green", linestyle = "-", marker = "o", makersize = 4)
+    ax.plot(x, y1, label = "cos(x)", color = "orange", linestyle = "--", marker = "x", makersize = 4)
+    
+    # Add some legend to the plot and render it.
+    ax.set_title("OO API Line Plot")
+    ax.set_xlabel("X-axis")
+    ax.set_ylabel("Y-axis")
+    ax.legend()
+    ax.grid(True)
+    fig.tight_layout()
+    # fig.savefig("line_plot_oo.png")   # Uncomment to save generated plot as png file.
+    plt.show()
+
+def dummyExample() -> None:
+    # Generate data to be plotted.
+    x: np.ndarray   = np.linspace(0, 10, 100)   # Generate 100 evenly spaced data points from 0 to 10.
+    y0: np.ndarray  = np.sin(x)                 # Execute sine function over points generated above.
+    y1: np.ndarray  = np.cos(x)                 # Same for cosine function.
+
+    # There are two main way make a plot: quick and API.
+    quickPlotting(x, y0, y1)
+    plotWithAPI(x, y0, y1)
 
 def main():
     dummyExample()
