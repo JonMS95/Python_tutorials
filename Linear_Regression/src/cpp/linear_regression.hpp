21b5ee6ef00ac53b747ee4c291183f886cd5298b
JonMS95 <jon_portugalete@hotmail.com>
2026-02-21 14:56:01 +0100

    Renamed API function as fitLeastSquares1F (where 1F stands for "1 feature").

--- Linear_Regression/src/cpp/linear_regression.hpp
+++ Linear_Regression/src/cpp/linear_regression.hpp
@@ -15,7 +15,7 @@ using vec_d = std::vector<double>;
 
 /******** Function prototypes *********/
 
-std::pair<double, double> fitLeastSquares2D(const vec_d& x, const vec_d& y);
+std::pair<double, double> fitLeastSquares1F(const vec_d& x, const vec_d& y);
 
 /**************************************/
 
