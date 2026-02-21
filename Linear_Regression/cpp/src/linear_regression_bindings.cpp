a36dc08b30faa26734c19299f0ef4ea98aa0a4d8
JonMS95 <jon_portugalete@hotmail.com>
2026-02-21 15:45:04 +0100

    Fixed file (curly brackets and colons missing).

--- Linear_Regression/cpp/src/linear_regression_bindings.cpp
+++ Linear_Regression/cpp/src/linear_regression_bindings.cpp
@@ -15,12 +15,12 @@ namespace py = pybind11;
 /********** Python bindings ***********/
 
 PYBIND11_MODULE(cpp_linear_regression, m)
+{
     m.doc() = "C++ linear regression module (least squares)";   // What's displayed when calling help(fitLinearData) in Python.
 
     m.def(  "fitLinearData"                             ,   // Python function name (the name the function to be called from the Python side will have).
             &fitLeastSquares1F                          ,   // Pointer to C++ function.
-            "Fit linear regression using least squares" )   // Python docstring.
+            "Fit linear regression using least squares" );  // Python docstring.
+}
 
 /**************************************/
-
-#endif
